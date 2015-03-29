df57003bbac759e8d2e5cd62b2a6c76c3e6a7077
Felipe Appio <felipexw@gmail.com>
2015-03-29 15:56:48 -0300

    O django-facebook funciona. Agora é só escrever um metodo para obter todas as publicacoes da timeline de um usuario e integrar com o nltk

--- tcc_appio/facebook/test.py
+++ tcc_appio/facebook/test.py
@@ -1,34 +1,38 @@
 from open_facebook.api import OpenFacebook
 
 
-ACCESS_TOKEN = "CAACEdEose0cBAJzgZCd18E80ZC3DsdnU3drVWQbZCZBNKuQ44BO4gRig8ygIsI8uBpToy5LO9Q1RtzuLiKd5zwIZByVIMaZCpCTtmoZANnZBz5wzOlr2ZCkhg5iNQu1qVzDMdxMu5AEVE52a3Bl1DPziiOZAlZCHdSsh9ewMFgzGuYz2ju5Htk0Ep5d0BPZA4IoSE1zezB1TGq7xfZAHJZCZAgZAQYZB2"
+ACCESS_TOKEN = "CAACEdEose0cBAAdyq7EIUZBNZAyJA902b7pJZBZARpmewwIKI9gysZC5hC5EcWgyVJWoqZBYciiiGpH5eVVIMIZB5reWgmaFZCWA6zNCgaZAsTgtvVEinGLv9jewbjPzZA8JIKFTDVWuKZAaLjKnLlstCwV4V9AUJsI4mSNXNSGT6ZADMKqZC1ZAqkQzgO8v3v9TSTIpdYaSqLuJeSixnbFFb2Y8wx"
 facebook = OpenFacebook(ACCESS_TOKEN)
-
-def get_statuses(facebook_object):
-    arr_statuses = []
-    statuses = facebook_object.get('me/statuses')
-    for s in statuses['data']:
-        arr_statuses.append(s['message'])
-
-    return arr_statuses
-
-def get_videos(facebook_object):
-    videos = facebook_object.get('me/photos')
-    
+def get_photos(facebook_object):
+    videos = facebook_object.get('me/feed')
     for i in videos['data']:
-        print i
         descricao_foto = i.get('name', 'none')
         array_comentarios = i.get('comments', 'null')
         print 'Descricao: ' + descricao_foto
-        
         if array_comentarios != 'null':
-            print '---comentariosss---'
-            array_data = array_comentarios['data']
-            for j in xrange(0, len(array_data)):
-                print 'autor do comentario: ' + array_data[j]['from']['name']
-                print 'comentario: ' + array_data[j]['message'] 
-                # nome do Carinha que fez o comentario: print array_data[j]['from']['name']
-                # comentario em si print array_data[j]['message']
-            
-get_videos(facebook)
+           print '---comentariosss---'
+           array_data = array_comentarios['data']
+           for j in xrange(0, len(array_data)):
+            print 'autor do comentario: ' + array_data[j]['from']['name']
+            print 'comentario: ' + array_data[j]['message']                
+def get_links(facebook_object):
+    data = facebook_object.get('me/links')
+    for post in data['data']:
+        print '\nautor: ' + post.get('from', 'none').get('name', 'none')
+        print 'nome: '  + post.get('name', 'none')
+        print 'link: ' + post.get('link', 'none')
+        print 'mensagem: ' + post.get('message', 'none')
+        print 'description: ' + post.get('description', 'none')
+        comments = post.get('comments', 'none')
 
+        if comments != 'none':
+           array_comments = comments.get('data', 'none')
+           print '\n---comentarios---'
+        
+        for i in xrange(0, len(array_comments)):
+            autor_comentario = array_comments[i].get('from','none').get('name', 'none')
+            mensagem_comentario = array_comments[i].get('message', 'none')
+            print 'autor: ' + autor_comentario
+            print 'comentario: ' + mensagem_comentario                
+        
+get_links(facebook)
