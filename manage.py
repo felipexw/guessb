16816e27cff8ef92b691e60da417f8d1c38b8509
Felipe Appio <felipexw@gmail.com>
2015-06-09 03:58:22 -0300

    s

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-
+from guessb.settings import BASE_DIR
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "guessb.settings")
+    path= os.path.join(BASE_DIR,  'guessb/dao')
+    sys.path.append(path)
 
     from django.core.management import execute_from_command_line
 
